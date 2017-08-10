1771bbccb11024ba8998e6783a3b821065e61211
CodeDad70 <eric.strickler@gmail.com>
2017-08-10 11:35:40 -0600

    added menu method to the CLI

--- lib/cooking_time/cli.rb
+++ lib/cooking_time/cli.rb
@@ -1,7 +1,22 @@
 class CookingTime::CLI
 
-  def welcome 
-    puts "hello"
+  def welcome
+    puts "It's Cooking Time !"
+    menu
+    # navigation
+    # goodbye
+  end
+
+  def menu 
+    puts <<-DOC
+     
+     How much time have you got ?
+
+     1. NO time - feed me now !!! (0 min.)
+     2. I have about 5 minutes keep it simple (2- 5 min.)
+     3. I'm not in a huge rush - but I don't have all night (10-15 min.)
+     4. Let's make something fancy pants (20+ min.)?
+     DOC
   end
 
 
