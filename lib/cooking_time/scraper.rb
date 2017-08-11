72db7caa412e05a7f53f44399cf841ef8dc0c81c
CodeDad70 <eric.strickler@gmail.com>
2017-08-11 12:09:09 -0600

    more work on the scraper - not working yet

--- lib/cooking_time/scraper.rb
+++ lib/cooking_time/scraper.rb
@@ -1,37 +1,32 @@
 class CookingTime::Scraper
-  attr_accessor :name, :difficulty, :url
+  attr_accessor :name, :url
 
-    
+    @all_recipes = []
 
     def self.recipes
+
       self.scrape_recipes
     end
 
-    # def self.collect_recipes
-    #   all_recipes = []
-    #   all_recipes << self.scrape_recipes
-    #   all_recipes
-    # end
-
     def self.scrape_recipes
-      recipes =[]
+      recipe = self.new
+      
       doc = Nokogiri::HTML(open("http://www.sofeminine.co.uk/world/cuisine/boitearecettes/id__t,5.html")) 
-      doc.search("td.af_baseS").each do |recipes|
-      
-      recipe_name = doc.search("a.br_textepetit").first.text.gsub("Recipe", "")
-      recipe_difficulty = doc.search("td.br_textepetit").first.text
-      recipe_url = doc.search("td.af_baseS a").attr("href").text
-      
-      recipes << {name: recipe_name, difficulty: recipe_difficulty, url: recipe_url}
-      
+      doc.css("td.af_baseS").each do |recipe_list|
+        recipe.name = recipe_list.search("a.br_textepetit").text
+        recipe.url = recipe_list.search("a").attr("href").text
+        @all_recipes << recipe
       end
-      recipes
-      binding.pry
     end
-
+  
   end
 
-
+  # doc.css("div#af_tdContenu td.af_baseS").each do |recipes|
   # name = doc.search("a.br_textepetit").first.text.gsub("Recipe", "")
   # difficulty = doc.search("td.br_textepetit").first.text
   #url = doc.search("td.af_baseS a").attr("href").text
+
+
+  #-------------------2nd try ----------------
+
+  # url = recipes.search("a").attr("href").text
