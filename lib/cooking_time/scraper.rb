0b16762487e4a8da4fb3e13361cbf1148e12147f
CodeDad70 <eric.strickler@gmail.com>
2017-08-10 22:20:52 -0600

    fixed the main recipe page  scrape to return recipe name, difficulty and url

--- lib/cooking_time/scraper.rb
+++ lib/cooking_time/scraper.rb
@@ -1,31 +1,39 @@
 class CookingTime::Scraper
-  attr_accessor :name, :difficulty, :time
+  attr_accessor :name, :difficulty, :url
+
+    @recipe = []
 
     def self.recipes
-      self.collect_recipes
+      self.scrape_recipes
     end
 
-    def self.collect_recipes
-      all_recipes = []
-      all_recipes << self.scrape_recipes
+    # def self.collect_recipes
+    #   all_recipes = []
+    #   all_recipes << self.scrape_recipes
+    #   all_recipes
+    # end
 
-      all_recipes
-    end
-
-    def self.scrape_recipes(time_select)
-      # doc = Nokogiri::HTML(open("http://www.sofeminine.co.uk/world/cuisine/boitearecettes/id__t,#{time_select}.html"))
-      recipe = self.new
-      puts "here are your recipes that are #{time_select} long"
-      # binding.pry
-
-      recipe
+    def self.scrape_recipes
+      doc = Nokogiri::HTML(open("http://www.sofeminine.co.uk/world/cuisine/boitearecettes/id__t,5.html"))
+      doc.each do |recipe|
+      @recipe = recipe
+      
+      recipe.name = doc.search("a.br_textepetit").first.text.gsub("Recipe", "")
+      recipe.difficulty = doc.search("td.br_textepetit").first.text
+      recipe.url = doc.search("td.af_baseS a").attr("href").text
+      
       
 
+      @all_recipes << recipe
+      binding.pry
+      
     end
-
+    recipe
+    end
 
   end
 
 
-  # name = doc.search("a.br_textepetit").first.text
+  # name = doc.search("a.br_textepetit").first.text.gsub("Recipe", "")
   # difficulty = doc.search("td.br_textepetit").first.text
+  #url = doc.search("td.af_baseS a").attr("href").text
